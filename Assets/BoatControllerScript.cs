fdf18b46c9efd97711ad5d9960767fdc20339ad9
Aleix Bueso <aleix.bueso@gmail.com>
2017-03-28 00:10:56 +0200

    First Commit

---
+++ Assets/BoatControllerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatControllerScript : MonoBehaviour {
+
+    float turnSpeed = 0.2f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+            gameObject.transform.Rotate( - Vector3.up * turnSpeed);
+
+        if (Input.GetKey(KeyCode.D))
+            gameObject.transform.Rotate (Vector3.up * turnSpeed);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Vector3 newPos = transform.position.x
+            //gameObject.transform.position = new Vector3(transform.position.x)
+        }
+
+
+    }
+}
