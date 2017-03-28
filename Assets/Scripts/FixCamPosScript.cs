5d79c7363c594b797a5e3dc0d35b3d0604e0b397
Aleix Bueso <aleix.bueso@gmail.com>
2017-03-28 16:50:58 +0200

    RigidBody Colision

---
+++ Assets/Scripts/FixCamPosScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixCamPosScript : MonoBehaviour {
+
+    public float Ypos = 6f;
+    private GameObject boat;
+
+	// Use this for initialization
+	void Start () {
+        boat = GameObject.Find("BoatController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position.Set(5, Ypos,9);
+	}
+}
