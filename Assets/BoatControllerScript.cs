238cf09b249f7f2a8e72f1a16b5e95f73003188f
Aleix Bueso <aleix.bueso@gmail.com>
2017-03-28 09:30:07 +0200

    Boat Movement

--- Assets/BoatControllerScript.cs
+++ Assets/BoatControllerScript.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class BoatControllerScript : MonoBehaviour {
 
-    float turnSpeed = 0.2f;
-	// Use this for initialization
-	void Start () {
+    public float turnSpeed = 0.2f;
+    public float turnAccel = 5f;
+
+    public float Speed = 0f;            //Don't touch this
+    public float MaxSpeed = 5f;        //This is the maximum speed that the object will achieve
+    public float Acceleration = 5f;    //How fast will object reach a maximum speed
+    public float Deceleration = 0.5f;    //How fast will object reach a speed of 0
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -18,11 +25,24 @@ public class BoatControllerScript : MonoBehaviour {
         if (Input.GetKey(KeyCode.D))
             gameObject.transform.Rotate (Vector3.up * turnSpeed);
 
-        if (Input.GetKey(KeyCode.W))
+        if ((Input.GetKey(KeyCode.W)))
         {
-            //Vector3 newPos = transform.position.x
-            //gameObject.transform.position = new Vector3(transform.position.x)
+            if (Speed < MaxSpeed)
+                Speed = Speed + Acceleration * Time.deltaTime;
+            else
+                Speed = MaxSpeed;
         }
+
+        else
+        {
+            if (Speed > 0)
+                Speed = Speed - Deceleration * Time.deltaTime;
+            else
+                Speed = 0;
+        }
+
+        float movement = Speed * Time.deltaTime;
+        gameObject.transform.Translate(0, 0, movement);
 
 
     }
