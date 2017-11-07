dc480713babe3a472cedc035956aa05c517eb97b
Aleix Bueso <aleix.bueso@gmail.com>
2017-11-07 13:59:12 +0100

    A light!

--- Assets/Scripts/TriangleData.cs
+++ Assets/Scripts/TriangleData.cs
@@ -1,49 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace BoatTutorial
+//To save space so we don't have to send millions of parameters to each method
+public struct TriangleData
 {
-    //To save space so we don't have to send millions of parameters to each method
-    public struct TriangleData
-    {
-        //The corners of this triangle in global coordinates
-        public Vector3 p1;
-        public Vector3 p2;
-        public Vector3 p3;
+	//The corners of this triangle in global coordinates
+	public Vector3 p1;
+	public Vector3 p2;
+	public Vector3 p3;
 
-        //The center of the triangle
-        public Vector3 center;
+	//The center of the triangle
+	public Vector3 center;
 
-        //The distance to the surface from the center of the triangle
-        public float distanceToSurface;
+	//The distance to the surface from the center of the triangle
+	public float distanceToSurface;
 
-        //The normal to the triangle
-        public Vector3 normal;
+	//The normal to the triangle
+	public Vector3 normal;
 
-        //The area of the triangle
-        public float area;
+	//The area of the triangle
+	public float area;
 
-        public TriangleData(Vector3 p1, Vector3 p2, Vector3 p3)
-        {
-            this.p1 = p1;
-            this.p2 = p2;
-            this.p3 = p3;
+	//The velocity of the triangle at the center
+	public Vector3 velocity;
 
-            //Center of the triangle
-            this.center = (p1 + p2 + p3) / 3f;
+	//The velocity normalized
+	public Vector3 velocityDir;
 
-            //Distance to the surface from the center of the triangle
-            this.distanceToSurface = Mathf.Abs(WaterController.current.DistanceToWater(this.center, Time.time));
+	//The angle between the normal and the velocity
+	//Negative if pointing in the opposite direction
+	//Positive if pointing in the same direction
+	public float cosTheta;
 
-            //Normal to the triangle
-            this.normal = Vector3.Cross(p2 - p1, p3 - p1).normalized;
+	public TriangleData(Vector3 p1, Vector3 p2, Vector3 p3, Rigidbody boatRB, float timeSinceStart)
+	{
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
 
-            //Area of the triangle
-            float a = Vector3.Distance(p1, p2);
+		//Center of the triangle
+		this.center = (p1 + p2 + p3) / 3f;
 
-            float c = Vector3.Distance(p3, p1);
+		//Distance to the surface from the center of the triangle
+		this.distanceToSurface = Mathf.Abs(WaterController.current.DistanceToWater(this.center, timeSinceStart));
 
-            this.area = (a * c * Mathf.Sin(Vector3.Angle(p2 - p1, p3 - p1) * Mathf.Deg2Rad)) / 2f;
-        }
-    }
+		//Normal to the triangle
+		this.normal = Vector3.Cross(p2 - p1, p3 - p1).normalized;
+
+		//Area of the triangle
+		this.area = BoatPhysicsMath.GetTriangleArea(p1, p2, p3);
+
+		//Velocity vector of the triangle at the center
+		this.velocity = BoatPhysicsMath.GetTriangleVelocity(boatRB, this.center);
+
+		//Velocity direction
+		this.velocityDir = this.velocity.normalized;
+
+		//Angle between the normal and the velocity
+		//Negative if pointing in the opposite direction
+		//Positive if pointing in the same direction
+		this.cosTheta = Vector3.Dot(this.velocityDir, this.normal);
+	}
 }
