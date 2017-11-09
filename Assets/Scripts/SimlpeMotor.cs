d802991049fe98bcd835a4b8b283acd627a527e1
Aleix Bueso <aleix.bueso@gmail.com>
2017-11-09 11:55:44 +0100

    New Physics!

---
+++ Assets/Scripts/SimlpeMotor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimlpeMotor : MonoBehaviour {
+
+	private Rigidbody BoatRB;
+
+	public float MotorForce = 100f;
+
+	// Use this for initialization
+	void Start () {
+		BoatRB = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W))
+			BoatRB.AddForce (new Vector3 (0, 0, -MotorForce));
+	}
+}
